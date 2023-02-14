f493734e38c811cbc6d313e43def1c46396ee162
zweix <1979803044@qq.com>
2023-02-14 11:46:00 +0800

    rewrite

--- README.md
+++ README.md
@@ -1,22 +1,41 @@
-```
-def run_help():
-    txt = """
-Usage: python3 md.py demo.md
-request: pip3 install markdown pyquery  pygments
+## Grammer
++ 水平幻灯片使用`\n---\n`（三个）分割
++ 垂直幻灯片使用`\n----\n`（四个）分割
++ fragment使用`\n--\n`（两个）分割
+    >更多样式见[reveal.js官网](https://revealjs.com/fragments/)
+    >其实又想通过`\n--x\n`的形式适配各种fragment，但是后来发现没有必要
 
-一些自定义解释标记
-https://revealjs.com/fragments/
-2个横线(--)  表示 fragment
-3个横线(---) 表示 section
-4个横线(----) 表示 vertical slides
-5个横线以上 md 默认展示方式 <hr>
-    
-"""
-    print(txt)
-    exit(0)
-```
+> 更多的`-`则是Mardown的`<br>`
 
++ Markdown原生语法适配：
+    + 注释
+    + 列表
+    + 代码和代码高亮
+        >reveal-md和slidev支持的特定行高亮不支持
+    + 引用
+    + 数学公式
+    + 表格（不够美观，后续修改）
 
-# 鸣谢
+## Install
+
+>需要使用Python3和Python3虚拟环境管理库poetry
+
++ 安装：`poetry install`
+
++ 使用：`poetry run python main.py`, 即在当前目录生成处理好的html文件
+
+## Quickstart
+
+安装后在项目目录下运行：
++ win:
+    ```bash
+    poetry run python main.py .\test\slide.md
+    ```
++ linux:
+    ```bash
+    poetry run python main.py ./test/slide.md
+    ```
+
+## 鸣谢
 + 感谢[南京大学蒋炎岩老师](https://ics.nju.edu.cn/~jyy/)录制了如此优质的[操作系统课程](https://jyywiki.cn/)
 + 感谢[顾若水](https://github.com/ruoshui255)提供的模板
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
+# coding=utf-8
 
-
-import os, sys
+import os
 import chardet
 import xml.etree.ElementTree as etree
 from pyquery import PyQuery as pq
@@ -9,8 +9,12 @@ import markdown
 from markdown import Extension
 from markdown.blockprocessors import BlockProcessor
 
+# settings
+template_html = str()
+template_html_from = "./static/template/template_c.html"
 
-#
+
+# infrastructure
 def get_file_code(file_path):  # 检测文件编码格式, 效率较低
     res = str()
     with open(file_path, "rb") as f:
@@ -18,7 +22,7 @@ def get_file_code(file_path):  # 检测文件编码格式, 效率较低
     return res
 
 
-def read(filepath):  # 读取文件所有内容
+def read(filepath):  # 读取文本文件内容
     if os.path.exists(filepath):
         with open(filepath, "r", encoding=get_file_code(filepath)) as f:
             content = f.read()
@@ -28,192 +32,165 @@ def read(filepath):  # 读取文件所有内容
         exit(-1)
 
 
-# 程序配置
-output_folder = "."
-template_html = read("btemplate.html")
+def write(filepath, data):  # 向文本文件(覆)写入内容
+    # with open(filepath, "wb") as f:
+    #     f.write(b"Hello World!")
+    # with open(filepath, "w", encoding=get_file_code(filepath)) as f:
+    #     f.write(data)
+    with open(filepath, "w", encoding="UTF-8") as f:
+        f.write(data)
 
 
-def write(data, filename):
-    file = os.path.join(output_folder, filename)
-    print("output name <{}>".format(file))
-
-    with open(file, "w+", encoding="utf-8") as f:
-        s = f.write(data)
-
-
+# div处理
 def add_wrap(e):
-    items = e("h1").parent()
-    # items = e('h1')
-
-    print("h1 {}".format(len(items)))
-    for item in items:
+    for item in e("h1").parent():
         t = pq(item)
         t.wrap("<div style='width:100%'>")
         t.wrap("<div class='center middle'>")
 
 
-class_data = {
-    "ul": " list-disc font-serif",
-    "li": " ml-8",
-    "h2": " text-xl mt-2 pb-2 font-sans",
-    "h1": " text-2xl mt-2 font-sans",
-    "p": " font-serif my-1",
-    "pre": " bg-gray-100 overflow-x-auto rounded p-2 mb-2 mt-2",
-}
-
-
 def add_class(e):
+    class_data = {
+        "ul": " list-disc font-serif",
+        "li": " ml-8",
+        "h2": " text-xl mt-2 pb-2 font-sans",
+        "h1": " text-2xl mt-2 font-sans",
+        "p": " font-serif my-1",
+        "pre": " bg-gray-100 overflow-x-auto rounded p-2 mb-2 mt-2",
+    }
     for k, v in class_data.items():
-        items = e(k)
-        for item in items:
+        for item in e(k):
             t = pq(item)
             t.add_class(v)
 
 
-def gen_html(md_html: str, output):
-    # print(md_html)
-    page = pq(md_html)
+# markdwon convert html
+def md_to_html(md: str) -> str:
+    # infrastructure
+    class BoxBlockProcessor(BlockProcessor):
+        first = True
+
+        def test(self, parent, block):
+            return self.first
+
+        def run(self, parent, blocks):
+            if self.first:
+                self.first = False
+
+                e = etree.SubElement(parent, "div")
+                # e.set('style', 'display: inline-block; border: 1px solid red;')
+                self.parser.parseBlocks(e, blocks)
+                # remove used blocks
+                for i in range(0, len(blocks)):
+                    blocks.pop(0)
+                return True  # or could have had no return statement
+            return False  # equivalent to our test() routine returning False
+
+    class BoxExtension(Extension):
+        def extendMarkdown(self, md):
+            md.parser.blockprocessors.register(BoxBlockProcessor(md.parser), "box", 175)
+
+    extensions = [
+        BoxExtension(),
+        "meta",
+        "fenced_code",
+        "codehilite",
+        "attr_list",
+        "tables",
+    ]
+    return markdown.markdown(md, extensions=extensions)
+
+
+def vertical_to_fragment(vertical: str) -> str:
+    fragment_sep = "\n--\n"
+    fragments = vertical.split(fragment_sep)
+
+    template = "<div class='fragment' data-fragment-index='{}'> {} </div>"
+    fragment_list = [md_to_html(fragments[0])]
+
+    for i in range(1, len(fragments)):
+        fragment_list.append(template.format(i - 1, md_to_html(fragments[i])))
+
+    return "\n".join(fragment_list)
+
+
+def horizontal_to_vertical(horizontal: str) -> str:
+    vertical_sep = "\n----\n"
+    verticals = horizontal.split(vertical_sep)
+
+    template = "\n<section>\n {} </section>"
+    if len(verticals) == 1:
+        template = "{}"
+
+    sections = list()
+    for vertical in verticals:
+        if vertical.isspace():
+            continue
+        fragmetns = vertical_to_fragment(vertical)
+        html = template.format(fragmetns)
+        sections.append(html)
+
+    return "\n".join(sections)
+
+
+def md_divide_to_horizontal(context: str):
+    horizontal_sep = "\n---\n"
+    horizontals = context.split(horizontal_sep)
+
+    sections = list()
+    template = "<section>\n {} \n</section>"
+
+    for horizontal in horizontals:
+        if horizontal.isspace():
+            continue
+        second_sections = horizontal_to_vertical(horizontal)
+        html = template.format(second_sections)
+        sections.append(html)
+
+    return "<html>\n<body>\n {} \t</body>\n</html>".format("\n".join(sections))
+
+
+def md_to_jyyhtml(context: str, filepath):
+    subject = md_divide_to_horizontal(context)
+
+    page = pq(subject)
     add_wrap(page)
     add_class(page)
 
     items = page("body").children()
-    print("div {}".format(len(items)))
 
+    global template_html
     template = template_html
     sections = "\n".join([str(pq(e)) for e in items])
-    res = template.replace("{}", sections)
-    # res = template.replace("{}", str(e))
 
-    write(res, output)
-
-
-class BoxBlockProcessor(BlockProcessor):
-    first = True
-
-    def test(self, parent, block):
-        return self.first
-
-    def run(self, parent, blocks):
-        if self.first:
-            self.first = False
-
-            e = etree.SubElement(parent, "div")
-            # e.set('style', 'display: inline-block; border: 1px solid red;')
-            self.parser.parseBlocks(e, blocks)
-            # remove used blocks
-            for i in range(0, len(blocks)):
-                blocks.pop(0)
-            return True  # or could have had no return statement
-        return False  # equivalent to our test() routine returning False
+    result = template.replace("{}", sections)
+    write(filepath, result)
+    pass
 
 
-class BoxExtension(Extension):
-    def extendMarkdown(self, md):
-        md.parser.blockprocessors.register(BoxBlockProcessor(md.parser), "box", 175)
+def process(file="./slide.md"):
+    filename = os.path.basename(file)
+    filepath = os.path.abspath(file)
+    output_filename = os.path.splitext(filename)[0] + ".html"
+    output_foldpath = filepath.split(filename)[0]
+    output_filepath = os.path.join(output_foldpath, output_filename)
 
+    global template_html
+    template_html = read(template_html_from)
+    title = ".".join(filename.split(".")[:-1])
+    template_html = template_html.replace("{{title}}", title)
 
-def md_to_html(md: str) -> str:
-    extensions = [BoxExtension(), "meta", "fenced_code", "codehilite", "attr_list"]
-    # html = markdown.markdown(text, extensions=[MyExtension()])
-    res = markdown.markdown(md, extensions=extensions)
-    return res
-
-
-def print_list(identifier: str, lst: list):
-    for t in lst:
-        print("\n{}".format(identifier))
-        print(t)
-        print("{}".format(identifier))
-
-
-def md_to_fragment(section: str):
-    """
-    2个横线
-    class="fragment" data-fragment-index="2
-    """
-
-    fragment_delimiter = "\n--\n"
-    fragments = section.split(fragment_delimiter)
-
-    res = [md_to_html(fragments[0])]
-
-    template = "<div class='fragment' data-fragment-index='{}'> {} </div>"
-    for i in range(1, len(fragments)):
-        t = template.format(i - 1, md_to_html(fragments[i]))
-        res.append(t)
-    return "\n".join(res)
-
-
-def md_to_vertical_section(vertical_md: str) -> str:
-    """
-    4个横线
-    表示section
-    """
-    md_delimiter = "\n----\n"
-    vertical_sections = vertical_md.split(md_delimiter)
-
-    template = "\n<section> {} </section>"
-    if len(vertical_sections) == 1:
-        template = "{}"
-
-    section = []
-    for t in vertical_sections:
-        fragments = md_to_fragment(t)
-        html = template.format(fragments)
-        section.append(html)
-    # print_list("--- section ---", section)
-
-    res = "\n".join(section)
-    return res
-
-
-def md_parse(md: str, output_file):
-    """
-    3个横线
-    表示 大 section
-
-    5个横线
-    表示 <hr>
-
-    """
-    big_section_delimiter = "\n---\n"
-    mds = md.split(big_section_delimiter)
-    print("mds[0] <{}>".format(mds[0]))
-
-    sections = []
-    template = "<section>\n {} \n</section>"
-    for t in mds:
-        if t.isspace():
-            continue
-        s = md_to_vertical_section(t)
-        html = template.format(s)
-        sections.append(html)
-    # print_list("*** big sections ***", sections)
-
-    # pq needs a parent
-    md_html = "<html><body>{}</body></html>".format("\n".join(sections))
-    gen_html(md_html, output_file)
+    context = read(filepath)
+    md_to_jyyhtml(context, output_filepath)
+    pass
 
 
 if __name__ == "__main__":
-    # if len(sys.argv) == 1:
-    #     run_help()
-    #     print("no input file")
-    #     exit(0)
-    #
-    # print(sys.argv)
-    # file = sys.argv[1]
-    # # data = get(file)
-    #
-    #
-    # name = file.split("/")[-1].split(".", 1)[0]
-    # if not name:
-    #     name = file.split("\\")[-1].split(".", 1)[0]
-    # print(f'name {name}')
-    #
-    # output_filename = "{}.html".format(name)
+    # process()
+    import sys
 
-    data = read("slide.md")
-    output_filename = "demo.html"
-
-    md_parse(data, output_filename)
+    for file in sys.argv[1:]:
+        print("working in {}".format(file), end=" ")
+        process(file)
+        print(" done")
+    pass
